41adda1764349289728566845c8b2cf5ef37764f
codesterkemp <sparky_csk@yahoo.com>
2013-05-11 14:14:35 -0400

    first Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SmallBiz::Application.config.secret_token = 'd6e1c80c818514fbe45ea9e001acf38165c522b8cd887009d555d69f83868e32280341eb9fa657219a364e69d0ba57a1c224e7d8ae0b3d5280b34c380fc9ec6a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+SmallBiz::Application.config.session_store :cookie_store, key: '_small__biz_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# SmallBiz::Application.config.session_store :active_record_store
